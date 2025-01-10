9e92ae9e01c673335bf59543e8b0db3eeef6688e
origami74@gmail.com <origami74@gmail.com>
2025-01-10 14:15:14 +0700

    add blossom upload

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@actions/core");
-console.log('Happy developing ✨');
+const NSecSigner_1 = require("@nostrify/nostrify/_dist/NSecSigner");
+const fs_1 = require("fs");
+const blossom_client_sdk_1 = require("blossom-client-sdk");
+console.log('Starting blossom Upload');
+const secretKey = new TextEncoder().encode("5de4e082b712da4364685141aa06b7d0fec9b178e1246c74dc66bc3dc03e5e61");
+const privateKeySigner = new NSecSigner_1.NSecSigner(secretKey);
+async function upload(filePath, host) {
+    const data = (0, fs_1.readFileSync)(filePath, 'utf-8');
+    const blob = new Blob([data], { type: 'text/plain' });
+    const client = new blossom_client_sdk_1.BlossomClient(host, (event) => privateKeySigner.signEvent(event));
+    const uploadAuthEvent = await client.createUploadAuth(blob, 'Upload file');
+    const result = await client.uploadBlob(blob, { auth: uploadAuthEvent });
+    const url = result.url;
+    console.log(result);
+    console.log(`Blob uploaded!, ${url}`);
+    return url;
+}
 try {
     // Fetch the value of the input 'who-to-greet' specified in action.yml
     const host = (0, core_1.getInput)('host');
-    console.log(`Uploading file to host: ${host}!`);
-    // Record the time of greeting as an output
-    const blossomHash = "ljsdlkfjhdslkhjfoiejofeljlksjlke";
-    (0, core_1.setOutput)("blossom-hash", blossomHash);
+    const filePath = (0, core_1.getInput)('filePath');
+    console.log(`Uploading file ${filePath} to host: ${host}!`);
+    upload(filePath, host)
+        .then(blossomHash => {
+        (0, core_1.setOutput)("blossom-hash", blossomHash);
+    })
+        .catch((error) => {
+        console.error("Blossom Upload failed with error", error);
+        if (error instanceof Error) {
+            (0, core_1.setFailed)(error.message);
+        }
+        else {
+            (0, core_1.setFailed)("unexpected error");
+        }
+    });
 }
 catch (error) {
-    console.log(error);
+    console.error("Blossom Upload failed with error", error);
     if (error instanceof Error) {
         (0, core_1.setFailed)(error.message);
     }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,55 @@
-import {setFailed, getInput, setOutput} from "@actions/core"
+import {getInput, setFailed, setOutput} from "@actions/core"
+import {NSecSigner} from "@nostrify/nostrify/_dist/NSecSigner";
+import {BlossomUploader} from "@nostrify/nostrify/_dist/uploaders/BlossomUploader";
+import { readFileSync } from 'fs';
+import {BlossomClient} from "blossom-client-sdk";
 
-console.log('Happy developing ✨')
+console.log('Starting blossom Upload');
+
+const secretKey: Uint8Array =new TextEncoder().encode("5de4e082b712da4364685141aa06b7d0fec9b178e1246c74dc66bc3dc03e5e61")
+const privateKeySigner = new NSecSigner(secretKey)
 
 
+async function upload(filePath: string, host:string): Promise<string>    {
+    const data = readFileSync(filePath, 'utf-8');
+    const blob = new Blob([data], {type: 'text/plain'});
+
+    const client = new BlossomClient(host, (event) => privateKeySigner.signEvent(event));
+
+    const uploadAuthEvent = await client.createUploadAuth(blob, 'Upload file')
+    const result = await client.uploadBlob(blob, {auth: uploadAuthEvent})
+
+    const url = result.url
+
+    console.log(result)
+    console.log(`Blob uploaded!, ${url}`);
+    return url;
+
+}
 
 try {
     // Fetch the value of the input 'who-to-greet' specified in action.yml
     const host = getInput('host');
-    console.log(`Uploading file to host: ${host}!`);
+    const filePath = getInput('filePath');
 
-    // Record the time of greeting as an output
-    const blossomHash = "ljsdlkfjhdslkhjfoiejofeljlksjlke"
-    setOutput("blossom-hash", blossomHash);
+    console.log(`Uploading file ${filePath} to host: ${host}!`);
+
+    upload(filePath, host)
+        .then(blossomHash => {
+            setOutput("blossom-hash", blossomHash);
+        })
+        .catch((error) => {
+            console.error("Blossom Upload failed with error", error);
+
+            if(error instanceof Error) {
+                setFailed(error.message);
+            } else{
+                setFailed("unexpected error");
+            }
+        })
 
 } catch (error) {
-    console.log(error);
+    console.error("Blossom Upload failed with error", error);
 
     if(error instanceof Error) {
         setFailed(error.message);
