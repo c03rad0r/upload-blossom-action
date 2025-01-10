ae7a08c141105e25fa0716e5639e729a27995526
origami74@gmail.com <origami74@gmail.com>
2025-01-10 15:19:22 +0700

    edit import blossom

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {getInput, setFailed, setOutput} from "@actions/core"
 import { readFileSync } from 'fs';
-import {BlossomClient} from "blossom-client-sdk";
 import {NSecSigner} from "@nostrify/nostrify/_dist/NSecSigner";
+import { BlossomClient } from "blossom-client-sdk/lib/client";
 
 console.log('Starting blossom Upload');
 
