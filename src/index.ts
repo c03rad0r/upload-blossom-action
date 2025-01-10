1602065034c68aeb0f74f1ee70c2caca381128bc
origami74@gmail.com <origami74@gmail.com>
2025-01-10 16:03:48 +0700

    ndk signer

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import {getInput, setFailed, setOutput} from "@actions/core"
 import { readFileSync } from 'fs';
-import {BlossomClient} from "blossom-client-sdk/lib";
 import {NDKPrivateKeySigner, NostrEvent} from "@nostr-dev-kit/ndk";
-import {EventTemplate, SignedEvent} from "blossom-client-sdk";
+import {EventTemplate, SignedEvent, BlossomClient} from "blossom-client-sdk";
 
 console.log('Starting blossom Upload');
 
