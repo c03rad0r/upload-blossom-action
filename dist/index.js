60b9c1e0e3c133880010c58ff6dbd824fb053deb
origami74@gmail.com <origami74@gmail.com>
2025-01-13 11:48:33 +0700

    fiddle with compiler options

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,18 @@
-import { getInput, setFailed, setOutput } from "@actions/core";
-import { readFileSync } from 'fs';
-import { NDKPrivateKeySigner } from "@nostr-dev-kit/ndk";
-import { BlossomClient } from "blossom-client-sdk";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const fs_1 = require("fs");
+const ndk_1 = require("@nostr-dev-kit/ndk");
+const blossom_client_sdk_1 = require("blossom-client-sdk");
 // import {EventTemplate, SignedEvent} from "blossom-client-sdk";
 console.log('Starting blossom Upload');
 const secretKey = new TextEncoder().encode("5de4e082b712da4364685141aa06b7d0fec9b178e1246c74dc66bc3dc03e5e61");
 // const privateKeySigner = new NSecSigner(secretKey)
 async function upload(filePath, host) {
-    const data = readFileSync(filePath, 'utf-8');
+    const data = (0, fs_1.readFileSync)(filePath, 'utf-8');
     const blob = new Blob([data], { type: 'text/plain' });
     async function signer(event) {
-        const signer = new NDKPrivateKeySigner(secretKey);
+        const signer = new ndk_1.NDKPrivateKeySigner(secretKey);
         const pubkey = await signer.user().then(u => u.pubkey);
         const signature = await signer.sign(event);
         const y = event;
@@ -27,7 +29,7 @@ async function upload(filePath, host) {
         // }
         return x;
     }
-    const client = new BlossomClient(host, signer);
+    const client = new blossom_client_sdk_1.BlossomClient(host, signer);
     //
     // const uploadAuthEvent = await client.createUploadAuth(blob, 'Upload file')
     // const result = await client.uploadBlob(blob, {auth: uploadAuthEvent})
@@ -40,29 +42,29 @@ async function upload(filePath, host) {
 }
 try {
     // Fetch the value of the input 'who-to-greet' specified in action.yml
-    const host = getInput('host');
-    const filePath = getInput('filePath');
+    const host = (0, core_1.getInput)('host');
+    const filePath = (0, core_1.getInput)('filePath');
     console.log(`Uploading file ${filePath} to host: ${host}!`);
     upload(filePath, host)
         .then(blossomHash => {
-        setOutput("blossom-hash", blossomHash);
+        (0, core_1.setOutput)("blossom-hash", blossomHash);
     })
         .catch((error) => {
         console.error("Blossom Upload failed with error", error);
         if (error instanceof Error) {
-            setFailed(error.message);
+            (0, core_1.setFailed)(error.message);
         }
         else {
-            setFailed("unexpected error");
+            (0, core_1.setFailed)("unexpected error");
         }
     });
 }
 catch (error) {
     console.error("Blossom Upload failed with error", error);
     if (error instanceof Error) {
-        setFailed(error.message);
+        (0, core_1.setFailed)(error.message);
     }
     else {
-        setFailed("unexpected error");
+        (0, core_1.setFailed)("unexpected error");
     }
 }
