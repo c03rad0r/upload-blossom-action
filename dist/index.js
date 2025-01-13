3a72935cc42dc9545d9f9822a65dd71c823719fd
origami74@gmail.com <origami74@gmail.com>
2025-01-13 13:27:02 +0700

    build

--- dist/index.js
+++ dist/index.js
@@ -53974,10 +53974,7 @@ _Mime_extensionToType = new WeakMap(), _Mime_typeToExtension = new WeakMap(), _M
 
 
 
-// import {EventTemplate, SignedEvent} from "blossom-client-sdk";
 console.log('Starting blossom Upload');
-// const secretKey: Uint8Array = new TextEncoder().encode("5de4e082b712da4364685141aa06b7d0fec9b178e1246c74dc66bc3dc03e5e61")
-// const privateKeySigner = new NSecSigner(secretKey)
 async function upload(filePath, host) {
     const data = (0,external_fs_.readFileSync)(filePath, 'utf-8');
     const fileType = dist_src.getType(filePath);
@@ -53987,19 +53984,12 @@ async function upload(filePath, host) {
         const pubkey = await signer.user().then(u => u.pubkey);
         const signature = await signer.sign(event);
         const y = event;
-        const x = { ...event, pubkey: pubkey, sig: signature, id: y.id };
-        return x;
+        return { ...event, pubkey: pubkey, sig: signature, id: y.id };
     }
     const client = new BlossomClient(host, signer);
-    //
     const uploadAuthEvent = await client.createUploadAuth(blob, 'Upload file');
     const result = await client.uploadBlob(blob, { auth: uploadAuthEvent });
-    //
-    const url = result.url;
-    //
-    // console.log(result)
-    console.log(`Blob uploaded!, ${url}`);
-    return url;
+    console.log(`Blob uploaded!, ${result.url}`);
 }
 try {
     // Fetch the value of the input 'who-to-greet' specified in action.yml
