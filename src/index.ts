85ec469e9ba3762c2330f4918c166c823b8e0074
origami74@gmail.com <origami74@gmail.com>
2025-01-13 13:25:49 +0700

    Some cleanup for v0.0.1 release

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,33 @@
 import {getInput, setFailed, setOutput} from "@actions/core"
-import { readFileSync } from 'fs';
+import {readFileSync} from 'fs';
 import {NDKPrivateKeySigner, NostrEvent} from "@nostr-dev-kit/ndk";
-import {BlossomClient, EventTemplate} from "blossom-client-sdk";
-import {SignedEvent} from "blossom-client-sdk";
+import {BlossomClient, EventTemplate, SignedEvent} from "blossom-client-sdk";
 import mime from "mime";
-// import {EventTemplate, SignedEvent} from "blossom-client-sdk";
 
 console.log('Starting blossom Upload');
 
-// const secretKey: Uint8Array = new TextEncoder().encode("5de4e082b712da4364685141aa06b7d0fec9b178e1246c74dc66bc3dc03e5e61")
-// const privateKeySigner = new NSecSigner(secretKey)
-
-
-async function upload(filePath: string, host: string): Promise<string> {
+async function upload(filePath: string, host: string): Promise<void> {
     const data = readFileSync(filePath, 'utf-8');
 
     const fileType = mime.getType(filePath);
     const blob = new Blob([data], {type: fileType?.toString()});
 
     async function signer(event: EventTemplate): Promise<SignedEvent> {
-
         const signer = new NDKPrivateKeySigner("5de4e082b712da4364685141aa06b7d0fec9b178e1246c74dc66bc3dc03e5e61");
         const pubkey = await signer.user().then(u => u.pubkey)
 
         const signature =  await signer.sign(event as NostrEvent);
 
         const y = event as NostrEvent
-        const x: SignedEvent = { ...event, pubkey: pubkey, sig: signature, id: y.id! };
-
-        return x;
+        return {...event, pubkey: pubkey, sig: signature, id: y.id!};
     }
 
     const client = new BlossomClient(host, signer);
-    //
-    const uploadAuthEvent = await client.createUploadAuth(blob, 'Upload file')
 
+    const uploadAuthEvent = await client.createUploadAuth(blob, 'Upload file')
     const result = await client.uploadBlob(blob, {auth: uploadAuthEvent})
-    //
-    const url = result.url
-    //
-    // console.log(result)
-    console.log(`Blob uploaded!, ${url}`);
-    return url;
+
+    console.log(`Blob uploaded!, ${result.url}`);
 }
 
 try {
@@ -73,5 +59,4 @@ try {
     } else{
         setFailed("unexpected error");
     }
-
 }
